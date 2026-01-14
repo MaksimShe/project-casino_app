b810f543360d8c088a3a481579964507e057597f
Maksym Shevchuk <Mr.Maks@i.ua>
2026-01-14 02:33:27 +0200

    fix: in deploy, when i open first time - i see jalousy, attempt2 to fix it

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -43,75 +43,72 @@ export default function Login() {
   };
 
   return (
-    <>
-      {/*<AnimatedBackground eyeState={isPasswordFocused} />*/}
-      <div className="flex h-[100vh] w-full items-center justify-center">
-        <div className="h-fill box-border flex w-[462px] flex-col items-center gap-8 rounded-[var(--main-radius)] bg-[#100F22] p-10 pt-4 max-sm:scale-85">
-          <div className="flex w-full flex-col items-center justify-center">
-            <Image src="/logo/logo.svg" alt="logo" width={96} height={96} />
-            <h1>Blaze casino</h1>
-            <h2>Welcome!</h2>
-            <form
-              onSubmit={handleSubmit(handleLogin)}
-              className="mt-10 flex w-full flex-col gap-2"
-            >
-              <div className="flex w-full flex-col justify-center gap-2">
-                <label htmlFor="email">Email</label>
-                <Input
-                  id="email"
-                  type="email"
-                  placeholder="Enter email"
-                  {...registerValue('email', {
-                    required: 'Email is required',
-                    pattern: {
-                      value: /^\S+@\S+$/,
-                      message: 'Invalid email',
-                    },
-                  })}
-                  disabled={isPending}
-                />
-              </div>
-
-              <div className="flex w-full flex-col justify-center gap-2">
-                <label htmlFor="password">Password</label>
-                <Controller
-                  name="password"
-                  control={control}
-                  rules={{ required: 'Password is required' }}
-                  render={({ field }) => (
-                    <PasswordInput {...field} disabled={isPending} />
-                  )}
-                />
-              </div>
-
-              <AuthError validationErrors={validationErrors} error={error} />
-
-              <button
-                type="submit"
+    <div className="-mt-[130px] flex h-[100vh] w-full items-center justify-center bg-gradient-to-b from-[#0F0C29] via-[#302B63] to-[#24243E]">
+      <div className="h-fill box-border flex w-[462px] flex-col items-center gap-8 rounded-[var(--main-radius)] bg-[#100F22] p-10 pt-4 max-sm:scale-85">
+        <div className="flex w-full flex-col items-center justify-center">
+          <Image src="/logo/logo.svg" alt="logo" width={96} height={96} />
+          <h1>Blaze casino</h1>
+          <h2>Welcome!</h2>
+          <form
+            onSubmit={handleSubmit(handleLogin)}
+            className="mt-10 flex w-full flex-col gap-2"
+          >
+            <div className="flex w-full flex-col justify-center gap-2">
+              <label htmlFor="email">Email</label>
+              <Input
+                id="email"
+                type="email"
+                placeholder="Enter email"
+                {...registerValue('email', {
+                  required: 'Email is required',
+                  pattern: {
+                    value: /^\S+@\S+$/,
+                    message: 'Invalid email',
+                  },
+                })}
                 disabled={isPending}
-                className="relative mt-8 flex h-12 w-full items-center justify-center rounded-3xl bg-gradient-to-t from-[#FF0047] to-[#FF417B] font-bold text-white transition-shadow duration-300 hover:shadow-[0_0_18px_0_#FF5A8C] disabled:cursor-not-allowed disabled:opacity-50"
-              >
-                {isPending ? 'Logging in...' : 'Login'}
-                <Image
-                  src="/logo/login.svg"
-                  alt="login logo"
-                  width={32}
-                  height={32}
-                  className="absolute right-2"
-                />
-              </button>
-            </form>
-          </div>
-          <div className="flex w-full flex-col items-center justify-center gap-4">
-            <p className="text-[#51A2FF] transition-colors duration-200 hover:text-[#AAD2FF]">
-              <a href={ROUTES.REGISTRATION}>Don`t have an account? Register</a>
-            </p>
-            <p className="w-full border-t pt-4 text-center text-xs">
-              Your account data is stored locally in your browser
-            </p>
-          </div>
+              />
+            </div>
+
+            <div className="flex w-full flex-col justify-center gap-2">
+              <label htmlFor="password">Password</label>
+              <Controller
+                name="password"
+                control={control}
+                rules={{ required: 'Password is required' }}
+                render={({ field }) => (
+                  <PasswordInput {...field} disabled={isPending} />
+                )}
+              />
+            </div>
+
+            <AuthError validationErrors={validationErrors} error={error} />
+
+            <button
+              type="submit"
+              disabled={isPending}
+              className="relative mt-8 flex h-12 w-full items-center justify-center rounded-3xl bg-gradient-to-t from-[#FF0047] to-[#FF417B] font-bold text-white transition-shadow duration-300 hover:shadow-[0_0_18px_0_#FF5A8C] disabled:cursor-not-allowed disabled:opacity-50"
+            >
+              {isPending ? 'Logging in...' : 'Login'}
+              <Image
+                src="/logo/login.svg"
+                alt="login logo"
+                width={32}
+                height={32}
+                className="absolute right-2"
+              />
+            </button>
+          </form>
+        </div>
+        <div className="flex w-full flex-col items-center justify-center gap-4">
+          <p className="text-[#51A2FF] transition-colors duration-200 hover:text-[#AAD2FF]">
+            <a href={ROUTES.REGISTRATION}>Don`t have an account? Register</a>
+          </p>
+          <p className="w-full border-t pt-4 text-center text-xs">
+            Your account data is stored locally in your browser
+          </p>
         </div>
       </div>
-    </>
+    </div>
   );
 }
--- src/app/registration/page.tsx
+++ src/app/registration/page.tsx
@@ -46,99 +46,96 @@ export default function Registration() {
   };
 
   return (
-    <>
-      {/*<AnimatedBackground eyeState={isPasswordFocused} />*/}
-      <div className="flex h-[100vh] w-full items-center justify-center">
-        <div className="h-fill box-border flex w-[462px] flex-col items-center gap-8 rounded-[var(--main-radius)] bg-[#100F22] p-10 pt-4 max-sm:scale-85">
-          <div className="flex w-full flex-col items-center justify-center">
-            <Image src="/logo/logo.svg" alt="logo" width={96} height={96} />
-            <h1>Blaze casino</h1>
-            <h2>Welcome!</h2>
-            <form
-              onSubmit={handleSubmit(handleRegister)}
-              className="mt-10 flex w-full flex-col gap-2"
-            >
-              <div className="flex w-full flex-col justify-center gap-2">
-                <label htmlFor="username">Username</label>
-                <Input
-                  id="username"
-                  type="text"
-                  placeholder="Enter username"
-                  disabled={isPending}
-                  {...registerValue('username', {
-                    required: 'Username is required',
-                    pattern: {
-                      value: /^[a-zA-Z0-9_]{4,11}$/,
-                      message: 'Username must be 4–11 characters long',
-                    },
-                  })}
-                />
-                {errors.username && (
-                  <p className="text-sm text-[var(--system-error-color)]">
-                    {errors.username.message}
-                  </p>
-                )}
-              </div>
-
-              <div className="flex w-full flex-col justify-center gap-2">
-                <label htmlFor="email">Email</label>
-                <Input
-                  id="email"
-                  type="email"
-                  placeholder="Enter email"
-                  {...registerValue('email', {
-                    required: 'Email is required',
-                    pattern: {
-                      value: /^\S+@\S+$/,
-                      message: 'Invalid email',
-                    },
-                  })}
-                  disabled={isPending}
-                />
-                {errors.email && (
-                  <p className="text-sm text-[var(--system-error-color)]">
-                    {errors.email.message}
-                  </p>
-                )}
-              </div>
-
-              <div className="flex w-full flex-col justify-center gap-2">
-                <label htmlFor="password">Password</label>
-                <Controller
-                  name="password"
-                  control={control}
-                  rules={{ required: 'Password is required' }}
-                  render={({ field }) => (
-                    <PasswordInput {...field} disabled={isPending} />
-                  )}
-                />
-                {errors.password && (
-                  <p className="text-sm text-[var(--system-error-color)]">
-                    {errors.password.message}
-                  </p>
-                )}
-              </div>
-              <AuthError validationErrors={validationErrors} error={error} />
-
-              <button
-                type="submit"
+    <div className="-mt-[130px] flex h-[100vh] w-full items-center justify-center bg-gradient-to-b from-[#0F0C29] via-[#302B63] to-[#24243E]">
+      <div className="h-fill box-border flex w-[462px] flex-col items-center gap-8 rounded-[var(--main-radius)] bg-[#100F22] p-10 pt-4 max-sm:scale-85">
+        <div className="flex w-full flex-col items-center justify-center">
+          <Image src="/logo/logo.svg" alt="logo" width={96} height={96} />
+          <h1>Blaze casino</h1>
+          <h2>Welcome!</h2>
+          <form
+            onSubmit={handleSubmit(handleRegister)}
+            className="mt-10 flex w-full flex-col gap-2"
+          >
+            <div className="flex w-full flex-col justify-center gap-2">
+              <label htmlFor="username">Username</label>
+              <Input
+                id="username"
+                type="text"
+                placeholder="Enter username"
                 disabled={isPending}
-                className="mt-8 h-12 w-full rounded-3xl bg-gradient-to-t from-[#FF0047] to-[#FF417B] font-bold text-white transition-shadow duration-300 hover:shadow-[0_0_18px_0_#FF5A8C] disabled:cursor-not-allowed disabled:opacity-50"
-              >
-                {isPending ? 'Creating account...' : 'Sign Up'}
-              </button>
-            </form>
-          </div>
-          <div className="flex w-full flex-col items-center justify-center gap-4">
-            <p className="text-[#51A2FF] transition-colors duration-200 hover:text-[#AAD2FF]">
-              <a href={ROUTES.LOGIN}>Already have an account? Login</a>
-            </p>
-            <p className="w-full border-t pt-4 text-center text-xs">
-              Your account data is stored locally in your browser
-            </p>
-          </div>
+                {...registerValue('username', {
+                  required: 'Username is required',
+                  pattern: {
+                    value: /^[a-zA-Z0-9_]{4,11}$/,
+                    message: 'Username must be 4–11 characters long',
+                  },
+                })}
+              />
+              {errors.username && (
+                <p className="text-sm text-[var(--system-error-color)]">
+                  {errors.username.message}
+                </p>
+              )}
+            </div>
+
+            <div className="flex w-full flex-col justify-center gap-2">
+              <label htmlFor="email">Email</label>
+              <Input
+                id="email"
+                type="email"
+                placeholder="Enter email"
+                {...registerValue('email', {
+                  required: 'Email is required',
+                  pattern: {
+                    value: /^\S+@\S+$/,
+                    message: 'Invalid email',
+                  },
+                })}
+                disabled={isPending}
+              />
+              {errors.email && (
+                <p className="text-sm text-[var(--system-error-color)]">
+                  {errors.email.message}
+                </p>
+              )}
+            </div>
+
+            <div className="flex w-full flex-col justify-center gap-2">
+              <label htmlFor="password">Password</label>
+              <Controller
+                name="password"
+                control={control}
+                rules={{ required: 'Password is required' }}
+                render={({ field }) => (
+                  <PasswordInput {...field} disabled={isPending} />
+                )}
+              />
+              {errors.password && (
+                <p className="text-sm text-[var(--system-error-color)]">
+                  {errors.password.message}
+                </p>
+              )}
+            </div>
+            <AuthError validationErrors={validationErrors} error={error} />
+
+            <button
+              type="submit"
+              disabled={isPending}
+              className="mt-8 h-12 w-full rounded-3xl bg-gradient-to-t from-[#FF0047] to-[#FF417B] font-bold text-white transition-shadow duration-300 hover:shadow-[0_0_18px_0_#FF5A8C] disabled:cursor-not-allowed disabled:opacity-50"
+            >
+              {isPending ? 'Creating account...' : 'Sign Up'}
+            </button>
+          </form>
+        </div>
+        <div className="flex w-full flex-col items-center justify-center gap-4">
+          <p className="text-[#51A2FF] transition-colors duration-200 hover:text-[#AAD2FF]">
+            <a href={ROUTES.LOGIN}>Already have an account? Login</a>
+          </p>
+          <p className="w-full border-t pt-4 text-center text-xs">
+            Your account data is stored locally in your browser
+          </p>
         </div>
       </div>
-    </>
+    </div>
   );
 }
